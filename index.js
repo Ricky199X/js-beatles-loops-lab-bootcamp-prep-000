c32708e83d16674c4bfdd93dfbb7f42722b155c9
Ricky Rojas <rickyrojas19@gmail.com>
2018-11-08 00:58:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments) {
   let beatlesArray = [];
 
   for(let i = 0; i < musicians.length; i ++) {
-    beatlesArray.push(musicians[i] + 'plays ' + instruments[i]);
+    beatlesArray.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return beatlesArray;
 }
